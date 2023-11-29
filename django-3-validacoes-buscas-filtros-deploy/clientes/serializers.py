723adfb3ebe4e77b525114176e318712d8ab7a57
Denilson Silva - Computador Tecnet <denilsonsilva@darksystem.com.br>
2023-11-29 16:39:51 -0300

    Melhorando o codigo

--- django-3-validacoes-buscas-filtros-deploy/clientes/serializers.py
+++ django-3-validacoes-buscas-filtros-deploy/clientes/serializers.py
@@ -1,24 +1,19 @@
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validators import *
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
-    def validate_cpf(self, cpf):
-        if len(cpf) != 11:
-            raise serializers.ValidationError("O CPF deve ter 11 dígitos")
-        return cpf
-    def validate_nome(self, nome):
-        if not nome.isalpha():
-            raise serializers.ValidationError("Apenas letras neste campo")
-        return nome
-    def validate_rg(self, rg):
-        if len(rg) != 9:
-            raise serializers.ValidationError("O RG deve conter 9 caracteres")
-        return rg
-    def validate_celular(self, celular):
-        if len(celular) < 11:
-            raise serializers.ValidationError("O deve conter 11 digitos")
-        return celular
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({ 'cpf': "O CPF deve ter 11 dígitos"})
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({ 'nome' : "Apenas letras neste campo"})
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({ 'rg' : "O RG deve conter 9 caracteres"})
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular' : "O deve conter 11 digitos"})
+        return data
         
