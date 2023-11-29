9134494231428dbc87fbac7c040113931f222d03
Denilson Silva - Computador Tecnet <denilsonsilva@darksystem.com.br>
2023-11-29 16:05:57 -0300

    Validando todos os campos

--- django-3-validacoes-buscas-filtros-deploy/clientes/serializers.py
+++ django-3-validacoes-buscas-filtros-deploy/clientes/serializers.py
@@ -9,4 +9,16 @@ class ClienteSerializer(serializers.ModelSerializer):
         if len(cpf) != 11:
             raise serializers.ValidationError("O CPF deve ter 11 dígitos")
         return cpf
+    def validate_nome(self, nome):
+        if not nome.isalpha():
+            raise serializers.ValidationError("Apenas letras neste campo")
+        return nome
+    def validate_rg(self, rg):
+        if len(rg) != 9:
+            raise serializers.ValidationError("O RG deve conter 9 caracteres")
+        return rg
+    def validate_celular(self, celular):
+        if len(celular) < 11:
+            raise serializers.ValidationError("O deve conter 11 digitos")
+        return celular
         
