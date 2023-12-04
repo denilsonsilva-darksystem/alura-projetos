1179e9ee979e2283fd9b4fda768e3b738fae51cc
Denilson Silva - Computador Tecnet <denilsonsilva@darksystem.com.br>
2023-12-04 17:34:58 -0300

    Cors no Django

--- api-django-3-versionamento-cabecalhos-cors/setup/settings.py
+++ api-django-3-versionamento-cabecalhos-cors/setup/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'escola',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',    
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -123,17 +125,21 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning',
-    'DEFAULT_AUTENTICATION_CLASSES': 'rest_framework.authentication.BasicAuthentication',
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
-        'rest_framework.permissions.DjangoModelPermissions'
-    ],
-    # 'DEFAULT_THROTTLE_CLASSES': [
-    #     'rest_framework.throttling.AnonRateThrottle',
-    #     # 'rest_framework.throttling.UserRateThrottle'
+    'DEFAULT_AUTENTICATION_CLASSES': 'rest_framework.authent;ication.BasicAuthentication',
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.IsAuthenticated', 
+    #     'rest_framework.permissions.DjangoModelPermissions'
     # ],
-    # 'DEFAULT_THROTTLE_RATES': {
-    #     'anon': '5/day',
-    #     # 'user': '1000/day'
-    # }
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        # 'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        # 'user': '1000/day'
+    }
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
