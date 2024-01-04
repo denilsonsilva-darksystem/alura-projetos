161eeac314f7b728d48163ee3e51910543a2c421
Denilson Silva <denilsonhto@gmail.com>
2024-01-04 16:31:20 -0300

    Testando PUT e DELETE

--- api-django-3-testes-seguranca-lapidacoes/test/tests_cursos.py
+++ api-django-3-testes-seguranca-lapidacoes/test/tests_cursos.py
@@ -8,10 +8,10 @@ class CursosTestCases(APITestCase):
     def setUp(self):
         self.list_url = reverse('Cursos-list')
         self.curso_1 = Curso.objects.create(
-            codigo_curso='CT1', descricao='Curso Teste 1', nivel='B'
+            codigo_curso='CTT1', descricao='Curso Teste 1', nivel='B'
         )
         self.curso_2 = Curso.objects.create(
-            codigo_curso='CT2', descricao='Curso Teste 2', nivel='A'
+            codigo_curso='CTT2', descricao='Curso Teste 2', nivel='A'
         )
 
     # def test_falhador(self):
@@ -25,9 +25,24 @@ class CursosTestCases(APITestCase):
     def test_requisicao_post_para_criar_curso(self):
         """Teste para verificar a requisição POST para criar um curso cursos"""
         data = {
-            'codigo_curso': 'CTTTd3',
+            'codigo_curso': 'CTT3',
             'descricao': 'Curso teste 3',
             'nivel': 'A'
         }
-        response = self.client.post(self.list_url, data)
+        response = self.client.post(self.list_url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_requisicao_delete_para_deletar_curso(self):
+        """Teste para verificar a requisição DELETE não permitida para deletar um curso"""
+        response = self.client.delete('/cursos/1/', format='json')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_requisicao_put_para_atualizar_curso(self):
+        """Teste para verificar requisição PUT para atualizar um curso"""
+        data = {
+            'codigo_curso': 'CTT1',
+            'descricao': 'Curso teste 1 atualizado',
+            'nivel': 'I'
+        }
+        response = self.client.put('/cursos/1/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
