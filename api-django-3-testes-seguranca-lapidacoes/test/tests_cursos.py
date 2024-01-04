0f8f2d3673136ff7271697243ccbaec420294336
Denilson Silva <denilsonhto@gmail.com>
2024-01-04 15:46:27 -0300

    Testando GET e POST

--- api-django-3-testes-seguranca-lapidacoes/test/tests_cursos.py
+++ api-django-3-testes-seguranca-lapidacoes/test/tests_cursos.py
@@ -1,7 +1,7 @@
 from rest_framework.test import APITestCase
 from escola.models import Curso
 from django.urls import reverse
-
+from rest_framework import status
 
 class CursosTestCases(APITestCase):
 
@@ -14,5 +14,20 @@ class CursosTestCases(APITestCase):
             codigo_curso='CT2', descricao='Curso Teste 2', nivel='A'
         )
 
-    def test_falhador(self):
-        self.fail('Falhou de proposito')
+    # def test_falhador(self):
+    #     self.fail('Falhou de proposito')
+
+    def test_requisicao_get_para_listar_cursos(self):
+        """Teste para verificar a requisição GET para listar os cursos"""
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_requisicao_post_para_criar_curso(self):
+        """Teste para verificar a requisição POST para criar um curso cursos"""
+        data = {
+            'codigo_curso': 'CTTTd3',
+            'descricao': 'Curso teste 3',
+            'nivel': 'A'
+        }
+        response = self.client.post(self.list_url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
