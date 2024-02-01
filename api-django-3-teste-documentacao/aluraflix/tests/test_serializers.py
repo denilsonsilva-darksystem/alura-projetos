dc2516c207fa2c7d08c280f0ff2f395b724fba01
Denilson Silva <denilsonhto@gmail.com>
2024-02-01 15:50:34 -0300

    Testando Serializer

---
+++ api-django-3-teste-documentacao/aluraflix/tests/test_serializers.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from aluraflix.models import Programa
+from aluraflix.serializers import ProgramaSerializer
+
+class ProgramaSerializerTestCase(TestCase):
+  
+  def setUp(self):
+    self.programa = Programa(
+      titulo = 'Procurando ninguém em latim',
+      data_lancamento = '2003-02-10',
+      tipo='F',
+      likes=2340,
+      dislikes=40
+    )
+    self.serializer = ProgramaSerializer(instance=self.programa)
+    
+  def test_verifica_campos_serializados(self):
+    """Teste que verifica que os campos que estão sendo serializados"""
+    data = self.serializer.data
+    self.assertEqual(set(data.keys()), set(['titulo', 'tipo', 'data_lancamento', 'likes']))
